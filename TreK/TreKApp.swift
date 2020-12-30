c8d2d1c139fd487f9c640de4c12cb75d39dff86a
Arjun <arjunmaganti10@gmail.com>
2020-12-29 22:17:11 -0800

    Initial Commit

---
+++ TreK/TreKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreKApp.swift
+//  TreK
+//
+//  Created by Arjun Maganti on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct TreKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
